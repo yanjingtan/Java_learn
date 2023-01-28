714facb70d6c447a54cc4fd83412ea03a4884064
kidyln114@gmail.com <kidyln114@gmail.com>
2023-01-29 06:44:09 +0800

    change package name to FirstTry_Jar

--- src/FirstTry_Jar/jar_test.java
+++ src/FirstTry_Jar/jar_test.java
@@ -1,7 +1,6 @@
 package FirstTry_Jar;
 public class jar_test {
     public static void main(String[] arg){
-
         System.out.println("这是带有图形化界面的程序");
         javax.swing.JOptionPane.showMessageDialog(null,"hello，很可爱","嘿嘿",javax.swing.JOptionPane.PLAIN_MESSAGE);
     }
